8ce4c6a45a6dbcf14a938c5384b89b8138fa0661
amomensah675 <amomensah675@gmail.com>
2025-11-29 17:11:28 +0000

    implement welcome section

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,10 +1,11 @@
-import Navbar from '#components/Navbar'
-import React from 'react'
+import Navbar from '#components/Navbar';
+import Welcome from '#components/Welcome';
 
 const App = () => {
   return (
-    <main className=''>
+    <main>
       <Navbar />
+      <Welcome />
     </main>
   )
 }
